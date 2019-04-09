481213c9ebc330d41c96f5de514901c9597ef353
Henrik Drefs <henrik.drefs@outlook.de>
2019-04-10 00:07:03 +0200

    fixed publish

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+export { default as Condition } from './Condition';
+
+export { default as Receipt, Address } from './Receipt';
+export { Time } from './Time';
+
 export { parseCondition } from './parse';
