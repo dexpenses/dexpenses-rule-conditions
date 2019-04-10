ca6561815dcb62f871944b7f939bad23a95439ec
Henrik Drefs <henrik.drefs@outlook.de>
2019-04-10 21:19:16 +0200

    add travis ci

--- sonar.js
+++ sonar.js
@@ -9,8 +9,7 @@ require('sonarqube-scanner')(
     options: {
       'sonar.organization': 'dexmo007-github',
       'sonar.sources': 'src/',
-      'sonar.exclusions':
-        'node_modules/**,sonar.js,test/generate.py,**/*.spec.ts',
+      'sonar.exclusions': 'node_modules/**,sonar.js,**/*.spec.ts',
       'sonar.typescript.tslint.reportPaths': '.sonar/tslint-report.json',
       'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
     },
